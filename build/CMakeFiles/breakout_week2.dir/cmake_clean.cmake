747deae7cf2b3a13e05ca8b74ab2ebd5fb09144b
GaoWeiFeng <2957618202@qq.com>
2026-04-03 10:54:49 +0800

    init: 初始化打砖块项目

---
+++ build/CMakeFiles/breakout_week2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/breakout_week2.dir/src/Ball.cpp.obj"
+  "CMakeFiles/breakout_week2.dir/src/Ball.cpp.obj.d"
+  "CMakeFiles/breakout_week2.dir/src/Brick.cpp.obj"
+  "CMakeFiles/breakout_week2.dir/src/Brick.cpp.obj.d"
+  "CMakeFiles/breakout_week2.dir/src/Paddle.cpp.obj"
+  "CMakeFiles/breakout_week2.dir/src/Paddle.cpp.obj.d"
+  "CMakeFiles/breakout_week2.dir/src/main.cpp.obj"
+  "CMakeFiles/breakout_week2.dir/src/main.cpp.obj.d"
+  "breakout_week2.exe"
+  "breakout_week2.exe.manifest"
+  "breakout_week2.pdb"
+  "libbreakout_week2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/breakout_week2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
